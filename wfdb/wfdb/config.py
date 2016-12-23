881640a271773839533442a2d6e33dc8238ccfe5
Aislan Max Gomes Coelho <marfebr@gmail.com>
2016-12-23 10:04:20 -0300

    Celery

--- wfdb/wfdb/config.py
+++ wfdb/wfdb/config.py
@@ -1,3 +1,5 @@
+from datetime import timedelta
+from celery.schedules import crontab
 class Config(object):
     SECRET_KEY = 'XMLZODSHE8N6NFOZDPZA2HULWSIYJU45K6N4ZO9M'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
@@ -22,8 +24,25 @@ class DevConfig(Config):
     CELERY_BROKER_URL = "redis://localhost:6379/0"
     CELERY_BACKEND_URL = "redis://localhost:6379/0"
 
-    
-
+    CELERYBEAT_SCHEDULE = {
+        'echo-every-5-seconds': {
+            'task': 'wfdb.tasks.echo',
+            'schedule': timedelta(seconds=5),
+            'args':("Hello World!!",)
+        },
+        'echo-crontab': {
+            'task': 'wfdb.tasks.echo',
+            'schedule': crontab(hour='15, 16, 17',minute='*/15'),
+            'args':("Hello World!!",)
+        },
+        'digest-every-sunday': {
+            'task': 'wfdb.tasks.digest',
+            'schedule': crontab(day_of_week=6, hour=23, minute=30),
+            'args':("Hello World!!",)
+        },
+        
+    }
+#schedule: hours=,minutes, seconds
 
 class TestConfig(Config):
     SQLALCHEMY_DATABASE_URI = 'sqlite://'
